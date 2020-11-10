a3cc7a453fccbe6bbacf1d318012d6c6e9ade165
taozheng4 <taozheng4@iflytek.com>
2020-11-10 13:53:04 +0800

    []

---
+++ src/main/java/com/iflytek/ebg/ct/ect/eureka/server/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.iflytek.ebg.ct.ect.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Bennu
+ * @date 2020/5/2
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
